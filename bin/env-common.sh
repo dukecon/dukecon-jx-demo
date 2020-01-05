5c230a06163f6e37e6b24a3aac3625c08b0dc98b
Gerd Aschemann <gerd@aschemann.net>
2020-01-05 11:34:04 +0100

    Distinguish JX installations by (install client) hostname
    and fix some shell problems

--- bin/env-common.sh
+++ bin/env-common.sh
@@ -3,10 +3,16 @@
 : ${JX:="jx"}
 export JX
 
-authfile=${JX_HOME:-"$HOME/.jx"}/gitAuth.yaml
+: ${JX_NAME:="jx-${HOSTNAME}"}
+export JX_NAME
+
+: ${JX_HOME:="$HOME/.jx"}
+export  JX_HOME
+
+authfile="${JX_HOME}/gitAuth.yaml"
 if test -r ${authfile}; then
-: ${JX_GIT_APITOKEN:=`yq r - servers[0].users[0].apitoken < ${authfile}`}
-: ${JX_GIT_USER:=`yq r - servers[0].users[0].username < ${authfile}`}
+: ${JX_GIT_APITOKEN:=$(yq r - servers[0].users[0].apitoken < ${authfile})}
+: ${JX_GIT_USER:=$(yq r - servers[0].users[0].username < ${authfile})}
 else
 : ${JX_GIT_APITOKEN:=""}
 : ${JX_GIT_USER:="${USER}"}
